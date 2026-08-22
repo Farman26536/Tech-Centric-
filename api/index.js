d67fcc7a467c9d2cf2ac9c0254ffca25f4c48847
Farman26536 <farmanullah26f@gmail.com>
2026-08-22 14:26:26 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
 import { app } from "../server/dist/app.js";
+import { connectDatabase } from "../server/dist/config/database.js";
 
-export default app;
+export default async function handler(req, res) {
+  try {
+    await connectDatabase();
+    return app(req, res);
+  } catch (error) {
+    console.error("API error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+}
